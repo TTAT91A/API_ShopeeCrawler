06b7fc943e67ee38e2677c50e1323248c7aaeaf8
TTAT91A <94796786+TTAT91A@users.noreply.github.com>
2023-11-01 03:22:28 +0700

    adjust main function

--- ShopeeCrawler.py
+++ ShopeeCrawler.py
@@ -94,10 +94,14 @@ def preprocessing(df):
     return df
 
 if __name__ == "__main__":
-    # os.mkdir('data')
-    filename_product = './data/product_' + str(date.today())+'.csv'
+        folder_path = os.path.join(os.path.dirname(__file__), 'data')
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    else:
+        pass
 
+    
     df_product = scrapeProduct()
     df_product = preprocessing(df_product)
 
-    df_product.to_csv(filename_product)
+    df_product.to_csv(folder_path + "/" + str(date.today())+ ".csv")
