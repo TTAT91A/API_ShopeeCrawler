bb4e1ddcc2426a61db8aea015f9c4b4c10fb08cc
TTAT91A <94796786+TTAT91A@users.noreply.github.com>
2023-12-05 01:15:33 +0700

    Update SaveToDatabase_MongoDB.py

--- SaveToDatabase_MongoDB.py
+++ SaveToDatabase_MongoDB.py
@@ -1,13 +1,12 @@
 import pandas as pd
 from pymongo import MongoClient
 from datetime import datetime
-
+from pre_processing import *
 # Function to import CSV to MongoDB
 # def import_csv_to_mongodb(csv_file, collection_name, database_name='Shopee', mongo_uri='mongodb://localhost:27017/'):
-def import_csv_to_mongodb(csv_file, collection_name, database_name='Shopee', mongo_uri='mongodb+srv://nattan1811:taibitri123@cluster0.voqacs7.mongodb.net/'):
+def import_csv_to_mongodb(df, collection_name, database_name='Shopee', mongo_uri='mongodb+srv://nattan1811:taibitri123@cluster0.voqacs7.mongodb.net/'):
 # mongodb+srv://nattan1811:<password>@cluster0.voqacs7.mongodb.net/
     # Read CSV into a Pandas DataFrame
-    df = pd.read_csv(csv_file)
 
     # Connect to MongoDB
     client = MongoClient(mongo_uri)
@@ -25,7 +24,9 @@ def import_csv_to_mongodb(csv_file, collection_name, database_name='Shopee', mon
 
 if __name__ == "__main__":
     str_today = str(datetime.today().date())
-
     path = 'https://raw.githubusercontent.com/TTAT91A/API_ShopeeCrawler/main/data/' + str_today + '.csv'
-    collection_name = "Shopee_API"
-    import_csv_to_mongodb(path, collection_name)
+    
+    collection_name = "Shopee"
+    df = pd.read_csv(path)
+    df = pre_processing(df)
+    import_csv_to_mongodb(df, collection_name)
