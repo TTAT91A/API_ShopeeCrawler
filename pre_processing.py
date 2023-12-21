1719dc9f1c8faed7db76372730eed8b27bfee42e
TTAT91A <94796786+TTAT91A@users.noreply.github.com>
2023-12-21 16:13:28 +0700

    append functions add fields: sold_in_day and revenue_in_day

--- pre_processing.py
+++ pre_processing.py
@@ -14,5 +14,32 @@ def pre_processing(df):
     df['brand'] = df['shop_name'].apply(lambda x: brand_mapping[x] if x in brand_mapping else x)
     return df
 
+def add_fields(current_day_df, previous_day_df): #add sold_per_day and revenue_per_day
+    processing_field = ["item_id","shop_name","historical_sold","price","crawl_date"]
+    merge_df = current_day_df[processing_field].merge(previous_day_df[processing_field],how='outer',on='item_id')
+    
+    #fillna 
+    merge_df["historical_sold_x"].fillna(0,inplace=True)
+    merge_df["historical_sold_y"].fillna(0,inplace=True)
+    merge_df["price_x"].fillna(0,inplace=True)
+    merge_df["price_y"].fillna(0,inplace=True)
+    
+    #calculate sold_per_day and revenue_per_day
+    merge_df['sold_in_day'] = merge_df['historical_sold_x'] - merge_df['historical_sold_y']
+    merge_df['revenue_in_day'] = merge_df['sold_in_day'] * merge_df['price_x']
 
-# pre_processing(df).to_csv("2023-11-29.csv")
+    #convert negative value in sold_per_day and revenue_per_day to 0
+    merge_df['sold_in_day'] = merge_df['sold_in_day'].apply(lambda x: max(0,x))
+    merge_df['revenue_in_day'] = merge_df['revenue_in_day'].apply(lambda x: max(0,x))
+
+    #astype
+    merge_df['sold_in_day'] = merge_df['sold_in_day'].astype('int64')
+    merge_df['revenue_in_day'] = merge_df['revenue_in_day'].astype('int64')
+    
+    #add to current_day_df
+    added_df = current_day_df.merge(merge_df[['item_id','sold_in_day','revenue_in_day']],on='item_id')
+    return added_df
+def add_fields_first_day(df):
+    df['sold_in_day'] = 0
+    df['revenue_in_day'] = 0
+    return df
